edaad4ec1a0fc634caf9d5ff29798a733381e3ef
Potzy12323123125y7 <alexander.naslund2@elev.ga.ntig.se>
2019-11-21 11:08:51 +0100

    2019-11-21 Alexander Jonsson Näsund te18

--- src/Medelvarde.java
+++ src/Medelvarde.java
@@ -5,23 +5,19 @@ import javax.swing.*;
 
 public class Medelvarde {
     public static void main(String[] args) {
-        int[] b = new int[10];
-        b[0] = Integer.parseInt(JOptionPane.showInputDialog(null, "det första talet?"));
-        b[1] = Integer.parseInt(JOptionPane.showInputDialog(null, "det andra talet?"));
-        b[2] = Integer.parseInt(JOptionPane.showInputDialog(null, "det tredje talet?"));
-        b[3] = Integer.parseInt(JOptionPane.showInputDialog(null, "det fjärde talet?"));
-        b[4] = Integer.parseInt(JOptionPane.showInputDialog(null, "det femte talet?"));
-        b[5] = Integer.parseInt(JOptionPane.showInputDialog(null, "det sjätte talet?"));
-        b[6] = Integer.parseInt(JOptionPane.showInputDialog(null, "det sjunde talet?"));
-        b[7] = Integer.parseInt(JOptionPane.showInputDialog(null, "det åttonde talet?"));
-        b[8] = Integer.parseInt(JOptionPane.showInputDialog(null, "det nionde talet?"));
-        b[9] = Integer.parseInt(JOptionPane.showInputDialog(null, "det tionde talet?"));
         int a = 0;
+        int c = 0;
+        c = Integer.parseInt(JOptionPane.showInputDialog(null,"Hur många tal?"));
+        int[] b = new int[c];
+        for (int q = 0; q < b.length; q++) {
+            b[q] = Integer.parseInt(JOptionPane.showInputDialog(null,"Skriv in ett av talen"));
+        }
+
         for (int i = 0; i < b.length; i++){
             a = b[i]+a;
 
         }
-    System.out.println(a/10);
+    System.out.println(a/c);
 
 
 
